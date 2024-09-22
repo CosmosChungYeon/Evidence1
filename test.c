50bc374babd3f40ec1db7ec3cce228c9a9ab89c8
ChungYeon <victor000417@gmail.com>
2024-09-22 21:34:37 +0900

    test

---
+++ test.c
@@ -0,0 +1,17 @@
+
+#include <stdio.h>
+
+#define MAX_WORD_LEN 2048
+
+typedef unsigned int word;
+
+typedef struct{
+    int sign;
+    int wordlen;
+    word BI_addr[MAX_WORD_LEN];
+} bigint;
+
+int main(){
+
+    return 0;
+}
