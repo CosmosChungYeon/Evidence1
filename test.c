90516fd202dd74d915a4369c85603c8d0ccde139
ChungYeon <victor000417@gmail.com>
2024-10-08 13:51:16 +0900

    homework

--- test.c
+++ test.c
@@ -1,17 +1,47 @@
-
 #include <stdio.h>
+#include <stdint.h>
 
-#define MAX_WORD_LEN 2048
+typedef uint32_t msg;
+typedef uint32_t word;
 
-typedef unsigned int word;
-
-typedef struct{
-    int sign;
-    int wordlen;
-    word BI_addr[MAX_WORD_LEN];
+typedef struct {
+		int sign;
+		int word_len;
+		word* a;
 } bigint;
 
-int main(){
+/* bigint <- array */
+msg bi_set_from_array(bigint* dst, int sign, int word_len, word* a);
 
-    return 0;
+/* bigint <- string */
+/* str = 0x123qqppqqwq 에러메시지 필요*/
+msg bi_set_from_string(bigint* dst, char* int_str, int base);
+
+/* A[4] = {1,2,3,0} -> {1,2,3,*1*} 마지막 원소가 nonzero여야 함 */
+msg bi_get_random(bigint* dst, int word_len);
+
+msg bi_print(bigint* dst, int base);
+
+msg bi_new(bigint* dst, int word_len);
+msg bi_delete(bigint* dst);
+
+msg bi_refine(bigint* dst); // 메모리 조정
+
+/* tmp <- x */
+msg bi_assign(bigint* dst, bigint* src);
+
+int main() {
+		//puts("HELLO KMU!!");
+		
+		bigint a, b, c;
+		/*
+				a = -0x123213129389213
+				b = 0x1fffff9389213
+				a = b
+		*/
+		bi_set_from_string(&a, "-0x123213129389213", 16);
+		bi_set_from_string(&b, "0x1fffff9389213", 16);
+		bi_assign(&a, &b);
+		
+		return 0;
 }
